c93f25d2eee9199ecc86849571a6269b70000672
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-07-18 15:59:25 -0300

    add profile update index routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import { Home } from '../pages/home'
 import { NotFound } from '../pages/notFound'
 import { Ejemplo } from '../pages/ejemplo'
 import { Login } from '../pages/login'
+import { Profile } from '../pages/profile'
 
 export const AppRoutes = () => {
   return (
@@ -13,7 +14,7 @@ export const AppRoutes = () => {
             <Route path='/drinksAppReact/' element={<Home/>}></Route>
             <Route path='*' element={<NotFound/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
-            <Route path='/user' element={Profile}>
+            <Route path='/user' element={ <Profile/> }>
 
             </Route>
             <Route path='/ejemplo' element={<Ejemplo/>}></Route>
